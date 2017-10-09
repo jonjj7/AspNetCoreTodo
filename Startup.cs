8f0308d9953d91559a00ea9a63533cafc1b599aa
Unknown <jonjj7@yahoo.com>
2017-10-08 20:14:51 -0500

    Security Chapter

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Helper;
 using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
 
@@ -49,12 +50,16 @@ namespace AspNetCoreTodo
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
         app.UseDatabaseErrorPage();
+
+        // Make sure there's a test admin account
+        EnsureRolesAsync(roleManager).Wait();
+        EnsureTestAdminAsync(userManager).Wait();
       }
       else
       {
@@ -72,5 +77,35 @@ namespace AspNetCoreTodo
                   template: "{controller=Home}/{action=Index}/{id?}");
       });
     }
+
+    private static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+    {
+      var alreadyExists = await roleManager.RoleExistsAsync(Constants.AdministratorRole);
+
+      if (alreadyExists)
+      {
+        return;
+      }
+
+      await roleManager.CreateAsync(new IdentityRole(Constants.AdministratorRole));
+    }
+
+    private static async Task EnsureTestAdminAsync(UserManager<ApplicationUser> userManager)
+    {
+      var testAdmin = await userManager.Users
+        .Where(x => x.UserName == "admin@todo.local")
+        .SingleOrDefaultAsync();
+
+      if (testAdmin != null)
+      {
+        return;
+      }
+
+      testAdmin = new ApplicationUser { UserName = "admin@todo.local", Email = "admin@todo.local" };
+      await userManager.CreateAsync(testAdmin, "NotSecure123!!");
+      await userManager.AddToRoleAsync(testAdmin, Constants.AdministratorRole);
+    }
+
+
   }
 }
